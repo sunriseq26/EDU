538795f3e5126ee8fa9ca556b402b42a21d98bb5
Ivan Kustarnikov <ikustarnikov@outlook.com>
2020-02-20 16:08:51 +0300

    Выполнил первый урок

---
+++ homeWork1/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = @"Здравствуйте уважаемы форумчане. 
+Попросили написать программу, которая считает количество 
+встречаемости каждой буквы в тексте.
+Подскажите как подсчитать эти смволы? Заранее спасибо!";
+        Console.WriteLine("Введите символ:");
+        string s = Console.ReadLine();
+        int count = str.ToCharArray().Where(c => c == s[0]).Count();
+        Console.WriteLine(count);
+        Console.ReadKey();
+
+
+    }
+}
