830a3764f4152e4b67ca3939e95c335569a2a02d
Ivan Kustarnikov <ikustarnikov@outlook.com>
2020-03-13 17:40:04 +0300

    Домашнее задание №6

---
+++ HomeWork2/CalcIMT/CalcIMT.cs
@@ -0,0 +1,73 @@
+﻿// Кустарников Иван
+// Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или все в норме;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcIMT
+{
+    class CalcIMT
+    {
+        static void Pause(string message)
+        {
+            Console.Write(message);
+            Console.ReadKey();
+        }
+
+        static double IMT(double m, double h)
+        {
+            double I = m / (Math.Pow(h, 2));
+            return I;
+        }
+
+        static void Main()
+        {
+            // Описание параметров консоли
+            Console.Title = "Кустарников Иван, Ставрополь";
+            Console.SetWindowSize(80, 30);
+            Console.SetBufferSize(80, 30);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            // Условия домашнего задания
+            Console.WriteLine("Кустарников Иван \n \n" +
+                "Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) " +
+                "по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах \n");
+            Pause("Нажмите любую клавишу, чтобы продолжить");
+            Console.Clear();
+
+            // Ввод данных и вычисление
+            Console.Write("Введите вашу массу тела в килограммах, разделитель точка: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\nВведите ваш рост в метрах, разделитель точка: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double I = weight / (Math.Pow(height, 2));
+
+
+            // Вывод результата используя форматированный вывод
+            Console.WriteLine("\nВаш индекс массы тела (ИМТ) равен - {0:N} \n", I);
+
+
+            if (I < 19)
+            {
+                Console.WriteLine("\nВам необходимо набрать вес");
+            }
+            if (I > 25)
+            {
+                Console.WriteLine("\nВам необходимо похудеть");
+            }
+            if (I > 19 && I < 25)
+            {
+                Console.WriteLine("\nВаш вес в норме");
+            }
+
+            Pause("\nДля выхода нажмите любую клавишу");
+        }
+    }
+}
---
+++ HomeWork2/LogPass/Program.cs
@@ -0,0 +1,109 @@
+﻿//Кустарников Иван
+
+//Реализовать метод проверки логина и пароля. На вход подается логин и пароль.
+//На выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains). 
+//Используя метод проверки логина и пароля, написать программу: пользователь вводит логин и пароль, программа пропускает его дальше или не пропускает. 
+//С помощью цикла do while ограничить ввод пароля тремя попытками.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMI_Stavropol
+{
+    namespace LogPass
+    {
+        class Program
+        {
+            // Метод проверки на наличие пользователя в БД.
+            public static bool userCheck(string user, string password)
+            {
+                // проверяем наличие пользователя
+                if (user == "root")
+                {
+                    Console.WriteLine("Пользователь найден!");
+                    if (passCheck(password) == true) // Проверяем пароль методом passCheck();
+                    {
+                        Console.WriteLine("Пароль правильный!");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Пароль не правильный!");
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: Пользователь не найден!");
+                    return false; // пользователя нет в БД.
+                }
+
+            }
+
+            // Метод проверки пароля пользователя
+            public static bool passCheck(string password)
+            {
+                if (password == "GeekBrains")
+                {
+                    return true; // пароль совпал
+                }
+                else return false; // пароль не совпал
+            }
+            static void Main()
+            {
+                int i = 1;
+                Console.WriteLine($"Попытка №: {i}"); // Вывод номера попытки ввода данных
+
+                Console.Write("Введите логин: ");
+                string userName = Console.ReadLine(); // Вводим логин
+
+                Console.Write("Введите пароль: ");
+                string userPassword = Console.ReadLine(); // Вводим пароль
+
+                // начинаем цикл do while
+                do
+                {
+                    Console.Clear(); // Чистка консоли
+
+                    if (userCheck(userName, userPassword) == false) // Проверка логина и пароля
+                    {
+                        if (i > 2) // Лимит попыток
+                        {
+                            Console.WriteLine("Доступ заблокирован: Превышен лимит попыток!");
+                            break; // выход из цикла
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Попытка №: {i + 1}"); // Вывод номера попытки ввода данных
+                            Console.WriteLine("Ошибка: Данные не верные!"); // Вывод ошибки
+
+                            Console.Write("Введите логин: ");
+                            userName = Console.ReadLine(); // Вводим логин
+
+                            Console.Write("Введите пароль: ");
+                            userPassword = Console.ReadLine(); // Вводим пароль
+                            i++; // операция инкремента
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Доступ открыт!");
+                        break; // выход из цикла
+                    }
+
+                } while (true); // вечный цикл
+
+
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
+
