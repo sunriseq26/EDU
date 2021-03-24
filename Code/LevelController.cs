75d40d06fe59907b2fb3ef531579fa8c08027b6b
Ivan Kustarnikov <ikustarnikov@outlook.com>
2021-03-24 15:56:07 +0300

    Move

---
+++ Code/LevelController.cs
@@ -0,0 +1,42 @@
+﻿//using Code.Player;
+using UnityEngine;
+using static UnityEngine.Debug;
+
+namespace Code 
+{
+    internal sealed class LevelController : MonoBehaviour
+    {
+        #region Field
+
+        private GameObject _player;
+        private GameObject _groundCheckObject;
+        private Rigidbody _rgPlayer;
+        private Animator _animatorPlayer = null;
+        private Transform _groundCheckTransform = null;
+        private LayerMask _whatIsGround;
+
+        #endregion
+            
+        void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag("Player");
+            Log(_player.name);
+            _rgPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+            Log(_rgPlayer.name);
+            _animatorPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+            Log(_animatorPlayer.name);
+            _whatIsGround = LayerMask.GetMask("Ground");
+            Log(_whatIsGround.value);
+            _groundCheckObject = GameObject.FindGameObjectWithTag("GroundCheck");
+            Log(_groundCheckObject.name);
+        }
+
+            
+        void Update()
+        {
+            _groundCheckTransform = _groundCheckObject.transform;
+            var playerMove = _player.GetComponent<Player.PlayerMovement>(); 
+            playerMove.MovePlayer(_player, _rgPlayer, _animatorPlayer, _groundCheckTransform, _whatIsGround);
+        }
+    }
+}
---
+++ Code/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+namespace Code.Player
+{
+    public class PlayerMovement : MonoBehaviour
+        {
+            #region Field
+
+            private float _speed = 4.0f;
+            private float _rotationSpeed = 40.0f;
+            private float _mouseSensitivity = 500.0f;
+            private float _jumpForce = 300.0f;
+            private float _groundRadius = 0.2f;
+            private float _rotation = 0f;
+            private float _mouseX = 0f;
+            private bool _isGrounded = false;
+            
+            
+            
+            private Vector3 _direction;
+
+            #endregion
+            
+            
+            #region Methods
+
+            public void MovePlayer(GameObject other, Rigidbody rgPlayer, 
+                Animator animatorPlayer, Transform groundCheck, 
+                LayerMask whatIsGround)
+            {
+                _direction.z = Input.GetAxis("Vertical");
+                _direction.x = Input.GetAxis("Horizontal");
+
+                if (Input.GetAxis("Mouse X") != 0)
+                {
+                    _mouseX = Input.GetAxis("Mouse X");
+                    other.transform.Rotate(new Vector3(0f, _mouseX * _mouseSensitivity * Time.deltaTime, 0f));
+                }
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                    Jump(other, rgPlayer, groundCheck, whatIsGround);
+                
+                var speed = _direction * _speed * Time.deltaTime;
+
+                if (speed != Vector3.zero)
+                    animatorPlayer.SetBool("isMove", true);
+                else
+                    animatorPlayer.SetBool("isMove", false);
+
+                var angleRotation = other.transform.rotation.eulerAngles.y + _rotation * _rotationSpeed * Time.deltaTime;
+        
+                other.transform.Translate(speed);
+                other.transform.rotation = Quaternion.Euler(0, angleRotation, 0); 
+            }
+
+            private void Jump(GameObject other, Rigidbody rgPlayer, 
+                Transform groundCheck, LayerMask whatIsGround)
+            {
+                _isGrounded = Physics.CheckSphere(groundCheck.position, 
+                    _groundRadius, whatIsGround);
+
+                if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+                {
+                    //var impulse = new Vector3(_direction.x, _jumpForce, _direction.z);
+                    var impulse = other.transform.up * rgPlayer.mass * _jumpForce;
+                    rgPlayer.AddForce(impulse);
+                }
+            }
+
+            #endregion
+        }
+}
